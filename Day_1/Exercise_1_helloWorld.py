8b02fc649bc97cb1a32c1cd06ac2cd5c080de54d
Aditya Nagariya <104499305+Aditya-Nagariya@users.noreply.github.com>
2023-08-30 12:42:43 +0530

    initial Commit

--- Day_1/Exercise_1_helloWorld.py
+++ Day_1/Exercise_1_helloWorld.py
@@ -1,9 +1,6 @@
 # Write a program in main.py that prints the same notes from the previous lesson using what you have learnt about the Python print function.
 
 print("Hello World!")
-
 print("Day 1 Of Python")
-
 print("The Function Is declared like This")
-
 print("print('What To Print')")
--- Day_1/Exercise_4_Variables.py
+++ Day_1/Exercise_4_Variables.py
@@ -2,25 +2,18 @@
 
 #Taking Input
 a = input("a: ")
-
 b = input("b: ")
 
 #Swaping Variables
 c = a
-
 a = b
-
 b = c
 
 #Display Result
 print("a: " + a)
-
 print("b: " + b)
 
 #Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.(using Variables)
-
 name = input("What's Your Name :- ")
-
 length = len(name)
-
 print("Your Name has " +(str)(length)+ " Characters")
